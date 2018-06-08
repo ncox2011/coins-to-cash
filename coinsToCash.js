c911cbaad930ddf63dbacb7d8e5c25fb25d93b75
Natasha Cox <natashac1014@gmail.com>
2018-06-08 12:03:14 -0500

    made it work

--- coinsToCash.js
+++ coinsToCash.js
@@ -1,1 +1,17 @@
-const piggyBank = {quarters: 5, nickels: 10, dimes: 20, pennies: 210,}
+const piggyBank = {quarters: 5, nickels: 10, dimes: 20, pennies: 210,};
+const coinValues = {quarterValue: 0.25, nickelValue: 0.05, dimeValue: 0.10, pennyValue: 0.01}
+let dollarAmount = 0;
+
+let totalQuarter = piggyBank.quarters*coinValues.quarterValue;
+let totalNickel = piggyBank.nickels*coinValues.nickelValue;
+let totalDimes = piggyBank.dimes*coinValues.dimeValue;
+let totalPenny = piggyBank.pennies*coinValues.pennyValue;
+
+dollarAmount = totalDimes+totalNickel+totalQuarter+totalPenny;
+console.log(dollarAmount);
+
+
+
+//let totalQuarter = function () {
+  //  quarters * quarterValue
+//}
